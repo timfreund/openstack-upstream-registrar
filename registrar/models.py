d3b7e960c0354781b9613e962f3b8ea048bc59fc
Tim Freund <tim@freunds.net>
2015-08-16 20:38:20 -0400

    startapp registar + v0 model

---
+++ registrar/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+class Contributor(models.Model):
+    user = models.OneToOneField(User, primary_key=True)
+    irc_nick = models.CharField(max_length=50)
+
+class Session(models.Model):
+    name = models.CharField(max_length=128)
+    maximum_students = models.IntegerField()
+    start_date = models.DateTimeField()
+    
+class StaffRegistration(models.Model):
+    contributor = models.ForeignKey(Contributor)
+    session = models.ForeignKey(Session)
+    mentor_capacity = models.IntegerField(default=0)
+    assistant = models.BooleanField(default=True)
+    instructor = models.BooleanField(default=True)
+
+class StudentRegistration(models.Model):
+    status_codes = (
+        ('active', 'Active'),
+        ('complete', 'Complete'),
+        ('new', 'New'),
+        ('waitlist', 'Wait list'),
+        ('withdrawn', 'withdrawn'),
+    )
+
+    contributor = models.ForeignKey(Contributor)
+    mentor = models.ForeignKey(Contributor, related_name='+')
+    session = models.ForeignKey(Session)
+
+    registration_date = models.DateTimeField()
+    status = models.CharField(choices=status_codes, max_length=128)
+
